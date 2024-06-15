de4fce2e197367921e16f955944b84e56296baee
SX <152792462+sx75@users.noreply.github.com>
2024-06-15 12:46:17 +0530

    Revert "lunaa: Build secure element service for 21603"
    This reverts commit c46b9a1c3951237110e2292e427006295fe50547.

--- device.mk
+++ device.mk
@@ -42,10 +42,8 @@ PRODUCT_PACKAGES += \
 # NFC
 PRODUCT_PACKAGES += \
     android.hardware.nfc-service.st \
-    android.hardware.secure_element-service.thales \
     android.hardware.secure_element@1.2.vendor \
     com.android.nfc_extras \
-    init.lunaa.rc \
     SecureElement \
     NfcNci \
     Tag
